68338c134738847d9aa54db0999c1a4a30cdf447
rick <technoweenie@gmail.com>
2009-12-19 17:31:19 -0800

    add github user feed parsing

--- lib/github_twitter_server/feeds.rb
+++ lib/github_twitter_server/feeds.rb
@@ -23,12 +23,40 @@ module GithubTwitterServer
     class AtomEntry
       include SAXMachine
       element :id, :as => :guid
-      element :content
       element :updated
-      element :author, :as => :author_name
+      element :author,  :as => :author_name
+      element :content, :as => :raw_content
+      element :link, :value => :href, :with => {:type => "text/html", :rel => 'alternate'}
+
+      def parsed_content
+        @parsed_content ||= begin
+          raw_content.gsub! /<(.|\n)+?>/, ''
+          raw_content.gsub! /\s+/, ' '
+          raw_content.strip!
+          raw_content
+        end
+      end
+
+      def content
+        @content ||= case event_type
+          when 'CommitCommentEvent'
+            parse_comment_event(parsed_content)
+          when 'PushEvent'
+            parse_push_event(parsed_content)
+          else
+            parsed_content
+        end
+      end
 
       def author
-        @author ||= author_name.strip
+        @author ||= begin
+          author_name.strip!
+          author_name
+        end
+      end
+
+      def event_type
+        @event_type ||= guid.scan(/\:(\w+)\/\d+$/)[0][0]
       end
 
       def status_id
@@ -37,6 +65,35 @@ module GithubTwitterServer
 
       def updated_at
         @updated_at ||= Time.parse(updated)
+      end
+
+      # Comment in a18f575: this mess is gonna get raw, like sushi =>
+      # @c_a18f575 this mess is gonna get raw, like sushi
+      def parse_comment_event(text)
+        # pull out commit hash and comment
+        if text =~ /(\w+)\: (.+)$/
+          "@c_#{$1} #{$2}"
+        else
+          text
+        end
+      end
+
+      # put each commit on a line
+      # @link users
+      # @link commits
+      def parse_push_event(text)
+        text = text.dup
+        # parse out "HEAD IS (sha)"
+        text.gsub! /^HEAD is \w+ /, ''
+        # [['technoweenie', 'sha1'], ['technoweenie', 'sha2']]
+        commits = text.scan(/(\w+) committed (\w+):/)
+        msgs    = text.split(/\w+ committed \w+: /)
+        msgs.shift
+        s = []
+        commits.each_with_index do |(user, sha), idx|
+          s << "@#{user} @c_#{sha} #{msgs[idx]}".strip
+        end
+        s * "\n"
       end
     end
 
--- test/feeds/user_feed_test.rb
+++ test/feeds/user_feed_test.rb
@@ -15,6 +15,30 @@ class UserFeedTest < FeedTestCase
       assert_equal 4, @feed.entries.size
     end
 
+    it "parses feed/entry/link" do
+      assert_equal 'http://github.com/technoweenie/faraday/commit/a18f5754eb01425aadf9ad27a20fd40422e8f3e2#-P0', @feed.entries[0].link
+    end
+
+    it "parses feed/entry/content for CommitCommentEvent" do
+      assert_equal '@c_a18f575 this mess is gonna get raw, like sushi', @feed.entries[0].content
+    end
+
+    it "parses feed/entry/content for CreateEvent" do
+      assert_equal 'New branch is at technoweenie/github_twitter_server/tree/master', @feed.entries[1].content
+    end
+
+    it "parses feed/entry/content for PushEvent" do
+      commit = "@technoweenie @c_a18f5754 add faraday gemspec"
+      assert_equal "#{commit}\n#{commit}", @feed.entries[3].content
+    end
+
+    it "parses event_types" do
+      assert_equal 'CommitCommentEvent', @feed.entries[0].event_type
+      assert_equal 'CreateEvent',        @feed.entries[1].event_type
+      assert_equal 'CreateEvent',        @feed.entries[2].event_type
+      assert_equal 'PushEvent',          @feed.entries[3].event_type
+    end
+
     it "parses feed/entry/id as :guid" do
       assert_equal 'tag:github.com,2008:CommitCommentEvent/113924968', @feed.entries[0].guid
     end
