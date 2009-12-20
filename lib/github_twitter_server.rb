93df478b26caf8cc627460abcdc9a3d9542989b3
rick <technoweenie@gmail.com>
2009-12-19 18:55:45 -0800

    add really basic github api wrapper

--- lib/github_twitter_server.rb
+++ lib/github_twitter_server.rb
@@ -1,3 +1,10 @@
+require 'faraday'
 module GithubTwitterServer
   autoload :Feed, "github_twitter_server/feed"
+
+  STATUS = {:source => 'github', :source_href => 'http://github.com'}.freeze
+
+  class Connection < Faraday::Connection
+    include Faraday::Adapter::Typhoeus.loaded? ? Faraday::Adapter::Typhoeus : Faraday::Adapter::NetHttp
+  end
 end
---
+++ lib/github_twitter_server/api.rb
@@ -0,0 +1,46 @@
+require 'sinatra'
+require 'twitter_server'
+require 'base64'
+require File.join(File.dirname(__FILE__), '../github_twitter_server')
+
+get '/' do
+  'hello world'
+end
+
+twitter_help
+
+twitter_basic_auth do |user, pass|
+  {:user => user.gsub(/^gh_/, ''), :token => pass}
+end
+
+# aka: 'wtf is tweetie sending me?'
+#
+#require 'pp'
+#get '/statuses/home_timeline.xml' do
+#  pp params
+#  pp env
+#end
+
+twitter_statuses_home_timeline do |params|
+  feed = github_user_feed(params[:auth][:user])
+  feed.atom.entries.map do |e|
+    GithubTwitterServer::STATUS.merge(:id => e.status_id, :text => e.content, :user => {:screen_name => e.author})
+  end
+end
+
+twitter_statuses_user_timeline do |params|
+  []
+end
+
+twitter_users_show do |params|
+  {}
+end
+
+twitter_account_verify_credentials do |params|
+  {:screen_name => params[:auth][:user]}
+end
+
+def github_user_feed(user)
+  conn = GithubTwitterServer::Connection.new("http://github.com")
+  GithubTwitterServer::Feed.new conn, "#{user}.atom"
+end
